bcbcee2eab8245404833e87bc5fc11a14b2e683a
igor7x7 <80541299+igor7x7@users.noreply.github.com>
2021-09-29 14:14:24 -0300

    injeção de html

---
+++ index.js
@@ -0,0 +1,18 @@
+var fields = document.querySelectorAll("#formulario-membros [name]");
+var user = {};
+
+fields.forEach(function (field) {
+  if (
+    field.name == "sexo" ||
+    field.name == "estadocivil" ||
+    field.name == "condicao"
+  ) {
+    if (field.checked) {
+      user[field.name] = field.value;
+    }
+  } else {
+    user[field.name] = field.value;
+  }
+});
+
+console.log(user);
