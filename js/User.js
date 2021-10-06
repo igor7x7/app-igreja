75e825e8f3de1075f716638ad4ac5df4548cbe89
igor7x7 <80541299+igor7x7@users.noreply.github.com>
2021-10-06 16:58:03 -0300

    controle navbar e estruturação de pastas

---
+++ js/User.js
@@ -0,0 +1,27 @@
+class User {
+
+    constructor(nome, sobrenome, sexo, escolaridade, civil, conjuge, nascimento, telefone, cep, logradouro, numero, complemento, bairro, estado, admicao, condicao, recebido, batismo, cargo, igreja, rg) {
+        this.nome = nome;
+        this.nome = sobrenome;
+        this.nome = sexo;
+        this.nome = escolaridade;
+        this.nome = civil;
+        this.nome = conjuge;
+        this.nome = nascimento;
+        this.nome = telefone;
+        this.nome = cep;
+        this.nome = logradouro;
+        this.nome = numero;
+        this.nome = complemento;
+        this.nome = bairro;
+        this.nome = estado;
+        this.nome = admicao;
+        this.nome = condicao;
+        this.nome = recebido;
+        this.nome = batismo;
+        this.nome = cargo;
+        this.nome = igreja;
+        this.nome = rg;
+    }
+
+}
---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function geral() {
+  frame = document.getElementById("frame");
+  frame.src = "/html/geral.html";
+}
+function membros() {
+  frame = document.getElementById("frame");
+  frame.src = "/html/formulario.html";
+}
